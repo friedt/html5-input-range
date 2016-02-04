9f6c658aa2c7af862d363025222fb9902bb8eb8d
Pepijn Friederichs <pepijn@dse.nl>
2016-02-04 06:27:59 +0100

    update html / grunt

--- gruntFile.js
+++ gruntFile.js
@@ -1,60 +1,67 @@
+// Load grunt tasks automatically
 
 
-	// Load grunt tasks automatically
+module.exports = function (grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+    // Project configuration.
+    grunt.initConfig({
+        less: {
+            development: {
+
+                files: {
+                    'dist/range-slider.css': 'css/*.less'
+                }
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 9002,
+                    base: ''
+                }
+            }
+        },
+
+        uglify: {
+            options: {
+                mangle: false
+            },
+            dist: {
+                files: {
+                    'dist/range-slider.min.js': ['javascript/*.js']
+                }
+            }
+        },
+        copy: {
+            html: {
+                src: 'html/range-slider.html',
+                dest: 'dist/index.html'
+            }
+        },
+        watch: {
+            css: {
+                files: ['css/*.less'],
+                tasks: ['less']
+            },
+            js:{
+                files: ['javascript/*.js'],
+                tasks: ['uglify']
+            },
+            html: {
+                files: ['html/*.html'],
+                tasks: ['copy']
+            },
+            options: {
+                livereload: true
+            }
+        }
+
+    });
 
 
-module.exports = function(grunt) {
-
-	require('load-grunt-tasks')(grunt);
-
-	// Project configuration.
-	grunt.initConfig({
-		less: {
-			development: {
-
-				files: {
-					'dist/range-slider.css': 'css/*.less'
-				}
-			}
-		},
-		connect: {
-			server: {
-				options: {
-					port: 9002,
-					base: ''
-				}
-			}
-		},
-		copy: {
-			html: {
-				src: 'html/range-slider.html',
-				dest: 'dist/index.html'
-			}
-		},
-		uglify: {
-			options: {
-				mangle: false
-			},
-			my_target: {
-				files: {
-					'dist/range-slider.min.js': ['javascript/*.js']
-				}
-			}
-		},
-		watch: {
-			css: {
-				files: ['**/*.less','**/*.js', '**/*.html'],
-				tasks: ['less','copy','uglify'],
-				options: {
-					livereload: true
-				}
-			}
-		}
-	});
-
-
-
-	// Default task(s).
-	grunt.registerTask('default', ['connect', 'less', 'uglify', 'watch']);
+    // Default task(s).
+    grunt.registerTask('default', ['connect', 'watch']);
 
 };
